120df899acb810d1c813e621851620a88fcd05b7
elmehdi elkabia <mahdielkabia@gmail.com>
2024-01-12 21:26:49 +0100

    mini-projet redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './component/Login';
+import Home from './component/Home';
+import { useSelector } from 'react-redux';
+import Notification from './component/Notification/Notification';
+import TodoAdd from './component/Todo/TodoAdd';
 
 function App() {
+  const authu = useSelector(state => state.isAuthenticated)
+  if (!authu) {
+    return <Login />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/todo" element={<TodoAdd />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
